c469be3809312ad6d02807e2a4e9c7ef36fd2f38
pedro99mendoza <pedromenfer123@gmail.com>
2023-07-15 18:21:50 +0200

    JavaScript project Pomodoro web

---
+++ main.js
@@ -0,0 +1,74 @@
+const tasks = [];
+let time = 0;
+let timer = null;
+let timerBreak = null;
+let current = null;
+
+const bAdd = document.querySelector("#bAdd")
+const itTask = document.querySelector("#itTask")
+const form = document.querySelector("#form")
+
+form.addEventListener("submit", e => {
+    e.preventDefault();
+    if(itTask.value != ""){
+        createTask(itTask.value)
+        itTask.value = "";
+        renderTasks();
+    }
+})
+
+function createTask(value){
+    const newTask = {
+        id: (Math.random() * 100).toString(36).slice(3),
+        title: value,
+        completed: false
+    };
+
+    tasks.unshift(newTask);
+}
+
+function renderTasks(){
+    const html = tasks.map(task => {
+        return `
+            <div class="task"> 
+                <div class="completed">${task.completed ? `<span class="done">Done</span>` :`<button class="start-button" data-id="${task.id}">Start</button>`}</div>
+                <div class="title">${task.title}</div>
+            </div>
+        `;
+    });
+
+    const taskContainer = document.querySelector("#tasks");
+    taskContainer.innerHTML = html.join('');
+
+    const startButtons = document.querySelectorAll('.task .start-button');
+    startButtons.forEach(button => {
+        button.addEventListener('click', e => {
+            if(!timer){
+                const id = button.getAttribute('data-id');
+                startButtonHandler(id);
+                button.textContent = 'In progress...'
+            }
+        });
+    });
+}
+
+function startButtonHandler(){
+    time = 25 * 60;
+    current = id;
+    const taskIndex = tasks.findIndex(task => task.id = id);
+    const taskName = document.querySelector('#time #taskName');
+    taskName.textContent = tasks[taskIndex].title;
+
+    timer = setInterval(() => {
+        timeHandler(id);
+    }, 1000);
+}
+
+function timeHandler(id){
+    time--;
+    renderTime();
+}
+
+function renderTime(){
+    
+}
